05746f38529bdbb5ac15dc888acd8911505c9a59
Lin Hugo <hugo970217@gmail.com>
2025-12-13 18:27:33 -0800

    feat: update layout metadata and modify README title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Analytics } from '@vercel/analytics/next'
 
-// @ts-expect-error types are not available yet?
 import { ViewTransition } from 'react'
 
 import cn from 'clsx'
@@ -31,8 +30,8 @@ const mono = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Shu Ding',
-    default: 'Shu Ding',
+    template: '%s - Lin Hugo',
+    default: 'Lin Hugo',
   },
 }
 
@@ -63,7 +62,7 @@ export default function RootLayout({
         <div className='fixed sm:hidden h-6 sm:h-10 md:h-14 w-full top-0 left-0 z-30 pointer-events-none content-fade-out' />
         <div className='flex flex-col mobile:flex-row'>
           <Navbar />
-          <main className='relative flex-1 max-w-2xl [contain:inline-size]'>
+          <main className='relative flex-1 max-w-2xl contain-[inline-size]'>
             <div className='absolute w-full h-px opacity-50 bg-rurikon-border right-0 mobile:right-auto mobile:left-0 mobile:w-px mobile:h-full mobile:opacity-100 mix-blend-multiply' />
             <ViewTransition name='crossfade'>
               <article className='pl-0 pt-6 mobile:pt-0 mobile:pl-6 sm:pl-10 md:pl-14'>
